3b0896a6845a094b710b4773174d77985d6d769d
FernanityChamp <5148837+fernanitychamp@users.noreply.github.com>
2018-06-19 20:41:12 -0500

    se agrego la libreria de html webpack para poder minificar y compilar todos los assets directamente en un archivo unico en formato html

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const webpack       = require('webpack');
 const path          = require('path');
 
-const MiniCssExtractPlugin  = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin             = require('html-webpack-plugin');
+const MiniCssExtractPlugin          = require('mini-css-extract-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
 const buildPath     = path.resolve(__dirname, 'build');
 const srcPath       = path.resolve(__dirname, 'src');
@@ -16,13 +18,20 @@ module.exports = {
     module: {
         rules: [
             { test: /\.(js|jsx)$/, include: srcPath, exclude: /(node_modules)/, use: 'babel-loader' },
-            { test: /\.css$/, include: srcPath + '/styles', exclude: /(node_modules)/, use: [ MiniCssExtractPlugin.loader, 'css-loader' ] }
+            { test: /\.css$/, include: srcPath + '/styles', exclude: /(node_modules)/, use: [ MiniCssExtractPlugin.loader, 'css-loader' ] },
+            { test: /\.html$/, include: srcPath, exclude: /(node_modules)/, use: 'html-loader' }
         ]
     },
     plugins: [
         new MiniCssExtractPlugin({
             filename: 'bundle.css',
             chunkFilename: '[id].css'
-        })
+        }),
+        new HtmlWebpackPlugin({
+            template: srcPath + '/app.html',
+            minify: true,
+            inlineSource: '.(js|css)$'
+        }),
+        new HtmlWebpackInlineSourcePlugin()
     ]
 }
