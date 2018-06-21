f906f072172cba663ed8ffbae4dd3b1d51f74998
FernanityChamp <5148837+fernanitychamp@users.noreply.github.com>
2018-06-21 11:41:01 -0500

    declarando un ganador

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -9,12 +9,34 @@ function Square(props) {
     )
 }
 
+function calcWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i]
+        if (squares[a] && squares[a] === squares[b] && squares[a] == squares[c]) {
+            return squares[a]
+        }
+    }
+    return null
+}
+
 
 export default class Board extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            playerTurn: 1
         }
     }
 
@@ -29,12 +51,24 @@ export default class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice()
-        squares[i] = 'X'
-        this.setState({ squares: squares })
+        if (calcWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.playerTurn == 1 ? 'X' : 'O'
+        this.setState({ 
+            squares: squares, 
+            playerTurn: this.state.playerTurn == 1 ? 2 : 1
+        })
     }
 
     render() {
-        const status = 'Siguiente jugador: X';
+        const winner = calcWinner(this.state.squares)
+        let status
+        if (winner) {
+            status = 'El Ganador es: ' + winner
+        } else {
+            status = 'Siguiente jugador: ' + (this.state.playerTurn == 1 ? 'X' : 'O');
+        }
 
         return (
             <div>
