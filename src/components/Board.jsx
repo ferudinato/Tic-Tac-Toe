209f9a8786415fe4910b01b079bf976dc7fc393d
FernanityChamp <5148837+fernanitychamp@users.noreply.github.com>
2018-06-21 11:05:48 -0500

    manejando estados y handler events

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,8 +4,26 @@ import Square from './Square.jsx';
 
 
 export default class Board extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            squares: Array(9).fill(null)
+        }
+    }
+
     renderSquare(i) {
-        return <Square />;
+        return (
+            <Square 
+                value={ this.state.squares[i] } 
+                onClick={ () => this.handleClick(i) } 
+            />
+        );
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice()
+        squares[i] = 'X'
+        this.setState({ squares: squares })
     }
 
     render() {
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 export default class Square extends React.Component {
     render() {
         return (
-            <button className='square'>{/* TODO */}</button>
+            <button className='square' onClick={() => this.props.onClick() }>
+                { this.props.value }
+            </button>
         )
     }
 }
