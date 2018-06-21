64aeb53f22ce9b79917fe81a109230de5a6cb999
FernanityChamp <5148837+fernanitychamp@users.noreply.github.com>
2018-06-20 22:18:19 -0500

    agregado los estilos necesarios que seran usados en la app. se agregaron tambien los componentes para el tablero y para renderear cada casilla

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,21 @@ import { render } from 'react-dom';
 
 import '../styles/app.css';
 
+import Board from './Board.jsx';
+
+
 export default class App extends React.Component {
     render() {
         return (
-            <h1>My React App!</h1>
+            <div className='game'>
+                <div className='game-board'>
+                    <Board />
+                </div>
+                <div className='game-info'>
+                    <div>{/* status */}</div>
+                    <div>{/* TODO */}</div>
+                </div>
+            </div>
         )
     }
 }
